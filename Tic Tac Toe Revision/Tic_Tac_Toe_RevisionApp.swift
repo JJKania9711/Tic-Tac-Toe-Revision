e2682c9a0c0c701a73add57363fa124b3d7c9188
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2022-11-13 11:18:53 -0800

    Initial commit

---
+++ Tic Tac Toe Revision/Tic_Tac_Toe_RevisionApp.swift
@@ -0,0 +1,18 @@
+//
+//  Tic_Tac_Toe_RevisionApp.swift
+//  Tic Tac Toe Revision
+//
+//  Created by James on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+
+struct Tic_Tac_Toe_RevisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
